91ef80a7da22f5a6df21f8170dcb0cca592c9255
JakubAniulisSensDx <jakub.aniulis@etongroup.eu>
2020-04-18 18:15:34 +0200

    works after homing in dobotstudio

--- dobotHandler.py
+++ dobotHandler.py
@@ -3,6 +3,11 @@ from serial.tools import list_ports
 import dobot
 import time
 
+#joint1 min -125 max 125
+#joint 2 min -5 max 90
+#j3 min - 15 max 90
+#joint4 min -150 max 150
+
 class dobotHandler:
     def __init__(self):
         port = list_ports.comports()[0].device
@@ -14,7 +19,8 @@ class dobotHandler:
 
 
 
-        device.move_to(x, y+20, z, r, wait=False)
+        device.speed()
+        device.move_to(x, y, z+20, r+100, wait=True)
         device._set_queued_cmd_start_exec()
         time.sleep(1)
         (x, y, z, r, j1, j2, j3, j4) = device.pose()
