c9f032ee5103ae65b2095aa08057356f783dcc61
JakubAniulisSensDx <jakub.aniulis@etongroup.eu>
2020-05-10 17:45:14 +0200

    Ploting 3d working

--- plotData.py
+++ plotData.py
@@ -11,15 +11,57 @@ from mpl_toolkits.mplot3d import Axes3D
 
 class plotData:
     def __init__(self):
-        self.fig, [[self.currentX,self.currentY,self.currentZ], [self.diffX, self.diffY, self.diffZ], [self.current3D, self.diff3D, self.predicted3D]] = plt.subplots(3, 3)
+        self.fig2D, [[self.currentX,self.currentY,self.currentZ], [self.diffX, self.diffY, self.diffZ]] = plt.subplots(2, 3)
+        self.fig3D = plt.figure(figsize=(3, 8))
+        self.current3D = self.fig3D.add_subplot(311, projection='3d')
+        self.diff3D = self.fig3D.add_subplot(312, projection='3d')
+        self.predicted3D = self.fig3D.add_subplot(313, projection='3d')
+
+        # handles, labels = ax.get_legend_handles_labels()
+        # title = 'Nyquist graph cmd: ' + str(cmdUsed) + " \nFrom " + str(start) + " took: " + str(
+        #     "{:.2f}".format(took)) + " s"
+        # ax.set_title(title)
+        # ax.set_xlabel('Z_Re [Ohms]')
+        # ax.set_ylabel('Z_Im [Ohms]')
+        #
+        # plt.legend(handles, labels, bbox_to_anchor=(1.05, 1), loc='upper left', prop={'size': 6})
 
         #ax = Axes3D(self.fig)
-    def subPlot(self, ax, data):
+    def subPlot(self, ax, data, color = None):
         ax.clear()
-        ax.plot(*data)
-    def subPlot3D(self, ax, data):
-        ax.clear()
-        ax.plot(*data)
+        if color is None:
+            ax.plot(*data)
+        else:
+            ax.plot(*data,color)
+
+    def subClear(self):
+        self.currentX.clear()
+        self.currentY.clear()
+        self.currentZ.clear()
+        self.diffX.clear()
+        self.diffY.clear()
+        self.diffZ.clear()
+        self.current3D.clear()
+        self.diff3d.clear()
+        self.predicted3d.clear()
+
+
+    def plot_figures(figures, nrows=1, ncols=1):
+        """Plot a dictionary of figures.
+
+        Parameters
+        ----------
+        figures : <title, figure> dictionary
+        ncols : number of columns of subplots wanted in the display
+        nrows : number of rows of subplots wanted in the figure
+        """
+
+        fig, axeslist = plt.subplots(ncols=ncols, nrows=nrows)
+        for ind, title in enumerate(figures):
+            axeslist.ravel()[ind].imshow(figures[title], cmap=plt.gray())
+            axeslist.ravel()[ind].set_title(title)
+            axeslist.ravel()[ind].set_axis_off()
+        plt.tight_layout()  # optional
 
     def plot(self, plotData, graphDataLengthToDisplay):
         gdl = None
@@ -31,15 +73,31 @@ class plotData:
         self.subPlot(self.currentY, [plotData['timestamp'],plotData['dobotY'],plotData['timestamp'],plotData['oculusY']])
         self.subPlot(self.currentZ, [plotData['timestamp'],plotData['dobotZ'],plotData['timestamp'],plotData['oculusZ']])
 
-        self.subPlot(self.diffX, [plotData['timestamp'],plotData['diffX']])
-        self.subPlot(self.diffY, [plotData['timestamp'],plotData['diffY']])
-        self.subPlot(self.diffZ, [plotData['timestamp'],plotData['diffZ']])
+        self.subPlot(self.diffX, [plotData['timestamp'],plotData['diffX']],color = 'g')
+        self.subPlot(self.diffY, [plotData['timestamp'],plotData['diffY']],color = 'g')
+        self.subPlot(self.diffZ, [plotData['timestamp'],plotData['diffZ']],color = 'g')
 
-        #self.subPlot3D(self.current3D, [plotData['dobotX'],plotData['dobotY'],plotData['dobotZ'],plotData['oculusX'],plotData['oculusY'],plotData['oculusZ']])
-        self.subPlot(self.diff3D, [plotData['timestamp'],plotData['dobotX']])
-        self.subPlot(self.predicted3D, [plotData['timestamp'],plotData['dobotX']])
-        self.fig.canvas.draw()
-        self.fig.canvas.flush_events()
+        self.subPlot(self.current3D, [plotData['dobotX'],plotData['dobotY'],plotData['dobotZ'],plotData['oculusX'],plotData['oculusY'],plotData['oculusZ']],color = 'g')
+        self.subPlot(self.diff3D, [plotData['diffX'],plotData['diffY'],plotData['diffZ']],color = 'g')
+        self.subPlot(self.predicted3D, [plotData['diffX'],plotData['diffY'],plotData['diffZ']],color = 'g')
+
+        self.currentX.set_title('Current X')
+        self.currentY.set_title('Current Y')
+        self.currentZ.set_title('Current Z')
+
+        self.diffX.set_title('Diff X')
+        self.diffY.set_title('Diff Y')
+        self.diffZ.set_title('Diff Z')
+
+        self.current3D.set_title('Current 3D')
+        self.diff3D.set_title('Diff 3D')
+        self.diff3D.set_title('Diff 3D')
+
+        self.fig2D.canvas.draw()
+        self.fig2D.canvas.flush_events()
+        plt.get_current_fig_manager().window.wm_geometry("+1000+100")
+        self.fig3D.canvas.draw()
+        self.fig3D.canvas.flush_events()
         self.firstPlot = False
 
         #self.fig.canvas.draw()
